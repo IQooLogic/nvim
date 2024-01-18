c8f6b123d97a7d1c0fb416b514c5ec17d1e28bca
milos <milos@ast.co.rs>
2024-01-18 15:45:20 +0100

    init

---
+++ lua/mappings.lua
@@ -0,0 +1,13 @@
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+vim.opt.autoindent = true
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.number = true
+
+vim.keymap.set('n', '<leader>w', ':write<CR>')
+vim.keymap.set('n', '<leader>q', ':quit<CR>')
+vim.keymap.set('n', '<leader>qq', ':q!<CR>')
