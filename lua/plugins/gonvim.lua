e39544400d8dbdee76f8a2c21666d4238f9c17f0
milos <milos@ast.co.rs>
2024-01-19 17:13:50 +0100

    added autocomplete and sinippets

---
+++ lua/plugins/gonvim.lua
@@ -0,0 +1,29 @@
+return {
+    "ray-x/go.nvim",
+    dependencies = {
+        "ray-x/guihua.lua",
+        "neovim/nvim-lspconfig",
+        "nvim-treesitter/nvim-treesitter",
+        "mfussenegger/nvim-dap",
+        "rcarriga/nvim-dap-ui",
+        "theHamsta/nvim-dap-virtual-text",
+    },
+    config = function()
+        require("go").setup()
+
+        local format_sync_grp = vim.api.nvim_create_augroup("GoImport", {})
+        vim.api.nvim_create_autocmd("BufWritePre", {
+            pattern = "*.go",
+            callback = function()
+                print("FORMATTING ...")
+                require("go.format").goimport()
+            end,
+            group = format_sync_grp,
+        })
+
+        vim.keymap.set("n", "<leader>ft", require("go.format").goimport, {})
+    end,
+    event = { "CmdlineEnter" },
+    ft = { "go", "gomod", "gowork", "gosum", "goimpl" },
+    build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
+}
