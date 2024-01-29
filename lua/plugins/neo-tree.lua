af1748ad7f60fee153b672917356c84e6363796d
milos <milos@ast.co.rs>
2024-01-29 16:20:26 +0100

    neotree show hidden files

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,15 +1,34 @@
 return {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-        "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-        "MunifTanjim/nui.nvim",
-    },
-    config = function()
-        vim.keymap.set("n", "<leader>n", ":Neotree filesystem reveal left<CR>", {})
-        vim.keymap.set("n", "<C-n>", ":Neotree toggle left<CR>", {})
-        vim.keymap.set("n", "<leader>nr", ":Neotree filesystem reveal right<CR>", {})
-        vim.keymap.set("n", "<leader>nt", ":Neotree toggle<CR>", {})
-    end,
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+		"MunifTanjim/nui.nvim",
+	},
+	opts = {
+		filesystem = {
+			filtered_items = {
+				--visible = true,
+				--show_hidden_count = true,
+				hide_dotfiles = false,
+				hide_gitignored = false,
+				hide_by_name = {
+					-- '.git',
+					-- '.DS_Store',
+					-- 'thumbs.db',
+				},
+				never_show = {},
+				always_show = { -- remains visible even if other settings would normally hide it
+					".gitignore",
+				},
+			},
+		},
+	},
+	config = function()
+		vim.keymap.set("n", "<leader>n", ":Neotree filesystem reveal left<CR>", {})
+		vim.keymap.set("n", "<C-n>", ":Neotree toggle left<CR>", {})
+		vim.keymap.set("n", "<leader>nr", ":Neotree filesystem reveal right<CR>", {})
+		vim.keymap.set("n", "<leader>nt", ":Neotree toggle<CR>", {})
+	end,
 }
