027b06df1334f40afa4d718913990f1a18d319bc
Milos Stojkovic <iqoologic@gmail.com>
2024-02-09 15:29:25 +0100

    Update indent-blankline to ignore dashboard

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -1,15 +1,26 @@
 return {
 	"lukas-reineke/indent-blankline.nvim",
-		event = "BufEnter",
-		main = "ibl",
-		opts = {},
-		config = function()
-			require("ibl").setup({
-				scope = {
-					enabled = false,
-					show_start = false,
-					show_end = false,
+	event = "BufEnter",
+	main = "ibl",
+	opts = {},
+	config = function()
+		require("ibl").setup({
+			scope = {
+				enabled = false,
+				show_start = false,
+				show_end = false,
+			},
+			exclude = {
+				filetypes = {
+					"lspinfo",
+					"packer",
+					"checkhealth",
+					"help",
+					"man",
+					"dashboard",
+					"",
 				},
-			})
-		end,
+			},
+		})
+	end,
 }
