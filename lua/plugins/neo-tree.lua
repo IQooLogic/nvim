7607b74f68d2135b1b2c6e28d88582a2574963a5
milos <milos@ast.co.rs>
2024-02-20 16:18:24 +0100

    neo-tree show sources

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -6,35 +6,70 @@ return {
 		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
 		"MunifTanjim/nui.nvim",
 	},
-	opts = {
-		filesystem = {
-			filtered_items = {
-				visible = true, -- when true, they will just be displayed differently than normal items
-				show_hidden_count = false,
-				hide_dotfiles = true,
-				hide_gitignored = true,
-				hide_hidden = true, -- only works on Windows for hidden files/directories
-				hide_by_name = {
-					--"node_modules"
+	opts = {},
+	config = function()
+		require("neo-tree").setup({
+			source_selector = {
+				winbar = true, -- toggle to show selector on winbar
+				statusline = false, -- toggle to show selector on statusline
+				show_scrolled_off_parent_node = true, -- boolean
+				sources = { -- table
+					{
+						source = "filesystem", -- string
+						display_name = " 󰉓 Files ", -- string | nil
+					},
+					{
+						source = "buffers", -- string
+						display_name = " 󰈚 Buffers ", -- string | nil
+					},
+					{
+						source = "git_status", -- string
+						display_name = " 󰊢 Git ", -- string | nil
+					},
 				},
-				hide_by_pattern = { -- uses glob style patterns
-					--"*.meta",
-					--"*/src/*/tsconfig.json",
-				},
-				always_show = { -- remains visible even if other settings would normally hide it
-					".gitignored",
-				},
-				never_show = { -- remains hidden even if visible is toggled to true, this overrides always_show
-					--".DS_Store",
-					--"thumbs.db"
-				},
-				never_show_by_pattern = { -- uses glob style patterns
-					--".null-ls_*",
+				content_layout = "start", -- string
+				tabs_layout = "equal", -- string
+				truncation_character = "…", -- string
+				tabs_min_width = nil, -- int | nil
+				tabs_max_width = nil, -- int | nil
+				padding = 0, -- int | { left: int, right: int }
+				separator = { left = "▏", right = "▕" }, -- string | { left: string, right: string, override: string | nil }
+				separator_active = nil, -- string | { left: string, right: string, override: string | nil } | nil
+				show_separator_on_edge = false, -- boolean
+				highlight_tab = "NeoTreeTabInactive", -- string
+				highlight_tab_active = "NeoTreeTabActive", -- string
+				highlight_background = "NeoTreeTabInactive", -- string
+				highlight_separator = "NeoTreeTabSeparatorInactive", -- string
+				highlight_separator_active = "NeoTreeTabSeparatorActive", -- string
+			},
+			filesystem = {
+				filtered_items = {
+					visible = true, -- when true, they will just be displayed differently than normal items
+					show_hidden_count = false,
+					hide_dotfiles = true,
+					hide_gitignored = true,
+					hide_hidden = true, -- only works on Windows for hidden files/directories
+					hide_by_name = {
+						--"node_modules"
+					},
+					hide_by_pattern = { -- uses glob style patterns
+						--"*.meta",
+						--"*/src/*/tsconfig.json",
+					},
+					always_show = { -- remains visible even if other settings would normally hide it
+						".gitignored",
+					},
+					never_show = { -- remains hidden even if visible is toggled to true, this overrides always_show
+						--".DS_Store",
+						--"thumbs.db"
+					},
+					never_show_by_pattern = { -- uses glob style patterns
+						--".null-ls_*",
+					},
 				},
 			},
-		},
-	},
-	config = function()
+		})
+
 		vim.keymap.set("n", "<leader>n", ":Neotree filesystem reveal left<CR>", {})
 		vim.keymap.set("n", "<C-n>", ":Neotree toggle left<CR>", {})
 		vim.keymap.set("n", "<leader>nr", ":Neotree filesystem reveal right<CR>", {})
