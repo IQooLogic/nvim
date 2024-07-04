8b5cff4ed5b20448e1b5aba6f0b53db69904e453
milos <milos@ast.co.rs>
2024-07-04 17:11:21 +0200

    changed theme to cyberdream

---
+++ lua/plugins/theme.lua
@@ -0,0 +1,111 @@
+return {
+	{
+		"scottmckendry/cyberdream.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			require("cyberdream").setup({
+				-- Enable transparent background
+				transparent = true,
+
+				-- Enable italics comments
+				italic_comments = false,
+
+				-- Modern borderless telescope theme
+				borderless_telescope = false,
+
+				-- Set terminal colors used in `:terminal`
+				terminal_colors = true,
+			})
+			vim.cmd("colorscheme cyberdream")
+		end,
+	},
+	-- {
+	-- 	"ellisonleao/gruvbox.nvim",
+	-- 	priority = 1000,
+	-- 	config = function()
+	-- 		require("gruvbox").setup({
+	-- 			terminal_colors = true, -- add neovim terminal colors
+	-- 			undercurl = true,
+	-- 			underline = true,
+	-- 			bold = true,
+	-- 			italic = {
+	-- 				strings = false,
+	-- 				emphasis = true,
+	-- 				comments = true,
+	-- 				operators = false,
+	-- 				folds = true,
+	-- 			},
+	-- 			strikethrough = true,
+	-- 			invert_selection = false,
+	-- 			invert_signs = false,
+	-- 			invert_tabline = false,
+	-- 			invert_intend_guides = false,
+	-- 			inverse = true, -- invert background for search, diffs, statuslines and errors
+	-- 			contrast = "hard", -- can be "hard", "soft" or empty string
+	-- 			palette_overrides = {},
+	-- 			overrides = {
+	-- 				SignColumn = { bg = "#1d2021" },
+	-- 				FoldColumn = { bg = "#1d2021" },
+	-- 			},
+	-- 			dim_inactive = false,
+	-- 			transparent_mode = false,
+	-- 		})
+	-- 		-- vim.cmd.colorscheme("gruvbox")
+	-- 	end,
+	-- },
+	-- {
+	-- 	"catppuccin/nvim",
+	-- 	name = "catppuccin",
+	-- 	priority = 1000,
+	-- 	config = function()
+	-- 		require("catppuccin").setup({
+	-- 			flavour = "mocha", -- latte, frappe, macchiato, mocha
+	-- 			background = { -- :h background
+	-- 				light = "latte",
+	-- 				dark = "mocha",
+	-- 			},
+	-- 			transparent_background = false, -- disables setting the background color.
+	-- 			show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
+	-- 			term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
+	-- 			dim_inactive = {
+	-- 				enabled = false, -- dims the background color of inactive window
+	-- 				shade = "dark",
+	-- 				percentage = 0.15, -- percentage of the shade to apply to the inactive window
+	-- 			},
+	-- 			no_italic = false, -- Force no italic
+	-- 			no_bold = false, -- Force no bold
+	-- 			no_underline = false, -- Force no underline
+	-- 			styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
+	-- 				comments = { "italic" }, -- Change the style of comments
+	-- 				conditionals = {},
+	-- 				loops = {},
+	-- 				functions = {},
+	-- 				keywords = {},
+	-- 				strings = {},
+	-- 				variables = {},
+	-- 				numbers = {},
+	-- 				booleans = {},
+	-- 				properties = {},
+	-- 				types = {},
+	-- 				operators = {},
+	-- 			},
+	-- 			color_overrides = {},
+	-- 			custom_highlights = {},
+	-- 			integrations = {
+	-- 				cmp = true,
+	-- 				gitsigns = true,
+	-- 				nvimtree = true,
+	-- 				treesitter = true,
+	-- 				notify = false,
+	-- 				mini = {
+	-- 					enabled = true,
+	-- 					indentscope_color = "",
+	-- 				},
+	-- 			},
+	-- 		})
+	--
+	-- 		-- vim.cmd.colorscheme("catppuccin")
+	-- 	end,
+	-- },
+}
